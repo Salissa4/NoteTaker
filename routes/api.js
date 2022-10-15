d67914e18f4bd72693f6fbe392bb9ab07ffad848
Monahlissa <salissa4@gmail.com>
2022-10-15 13:28:14 -0500

    create routes

---
+++ routes/api.js
@@ -0,0 +1,49 @@
+const route = require('express').Router();
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+//Get route for note retrieval
+route.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+//post route for feedback
+route.post('/notes', (req, res) => {
+    //deconstruct items in req.body
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Post feedback error!');
+    }
+});
+
+route.delete('/notes/:noteId', (req, res) => {
+    const noteId = req.params.noteId;
+    readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        const data = json.filter((note) => note.noteId !== noteId);
+
+        writeToFile('./db/db.json',data);
+
+        res.json('Note ${noteID} has been deleted');
+    });
+});
+
+module.exports = route;
+
