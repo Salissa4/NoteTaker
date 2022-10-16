e82b17cccb2aae8f475a7fa3d8d52ec4a9672d3f
Monahlissa <salissa4@gmail.com>
2022-10-16 11:37:00 -0500

    created routes, store.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
-const api = require('./routes/api');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,20 +9,12 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
+//PORT
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
